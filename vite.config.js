072c098d51d3b693aacf33ad8e379a29f6be0327
Bhoomika Shetty <bhoomikashetty30@gmail.com>
2025-10-12 22:54:48 +0530

    sixth commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 // vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/arghya-proj/',   // ← MUST match your repo name
+  // ADD or UPDATE this line:
+  base: '/',
+  //
   plugins: [react()],
 })
